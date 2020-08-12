bca92c90db13db5591633d33f5202cfa7a15bfd7
unknown <you@example.com>
2020-08-12 08:37:05 +0700

    upbai

---
+++ module1/7.bien_kieu_du_lieu_va_toan_tu/thuc_hanh/js_su_dung_toan_tu.js
@@ -0,0 +1,8 @@
+let Width = parseInt(prompt('Enter the width'));
+let Height = parseInt(prompt('Enter the height'));
+let area = Width * Height ;
+document.write("Enter the width: " + Width);
+document.write('<br/>');
+document.write("Enter the height: " + Height);
+document.write('<br/>');
+document.write("The area is: " + area);
